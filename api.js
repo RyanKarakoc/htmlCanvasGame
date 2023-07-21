5eac8aa397244dc5a606cf363923f3da6d276ea4
Ryan <kochy@hotmail.co.uk>
2023-07-21 14:01:24 +0100

    api functions all work

--- api.js
+++ api.js
@@ -1,17 +1,36 @@
-import axios from "axios";
+const axios = require("axios");
 
 const highscoresAPI = axios.create({
-  baseURL: "",
+  baseURL: "https://htmlcanvasgamebackend.onrender.com/api",
 });
 
-export const fetchHighscores = () => {
-  return highscoresAPI.get("/highscores").then((data) => {
-    return response.data.highscores;
+const fetchHighscores = () => {
+  return highscoresAPI.get("/highscores").then((response) => {
+    console.log(response.data);
+    return response.data;
   });
 };
 
-export const fetchHighscoresByUsername = (username) => {
-    return highscoresAPI.get(`/highscores/${username}`).then((data) => {
-      return response.data.highscores;
+const fetchHighscoresByUsername = (username) => {
+  return highscoresAPI
+    .get(`/highscore?username=${username}`)
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
     });
+};
+
+const postNewHighscore = (username, score) => {
+  postObject = {
+    username: username,
+    score: score,
   };
+  return highscoresAPI.post("/highscore", postObject).then((response) => {
+    console.log(response.data);
+    return response.data;
+  });
+};
+
+// postNewHighscore("mike", 12);
+// fetchHighscores();
+fetchHighscoresByUsername("eggo");
